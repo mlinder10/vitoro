2325fdc47cce507e8922bb0bdcfa9b8e679915ec
Matt Linder <108966946+mlinder10@users.noreply.github.com>
2025-06-18 22:42:43 -0400

    added test for hosting platform

--- src/app/api/env-test/route.ts
+++ src/app/api/env-test/route.ts
@@ -1,3 +1,6 @@
-export async function GET() {
-  return new Response(process.env.JWT_KEY);
+export async function GET(req: Request) {
+  // get query param: "key"
+  const { searchParams } = new URL(req.url);
+  const key = searchParams.get("key");
+  return new Response(`${key}: ${process.env[key ?? ""]}`);
 }
